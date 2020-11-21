310c4dc6c57e282033bf03f2e002f58ab72ece68
damhuman <anton.dasyuk@gmail.com>
2020-11-21 13:50:07 +0200

    heroku configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn meter_ssolutions.wsgi
--- meter_solutions/settings.py
+++ meter_solutions/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dj_database_url
+import django_heroku
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())

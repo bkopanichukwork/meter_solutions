320b15dabeb6724f621bc30c197bb52dc87a00a2
Bohdan Kopanichuk <danbog2000@gmail.com>
2020-11-27 01:24:28 +0200

    handlers init

--- apps/comunicator/services/connector.py
+++ apps/comunicator/services/connector.py
@@ -3,12 +3,13 @@ import time
 
 from paho.mqtt import client as mqtt_client
 
-from apps.comunicator.services.decoders import Zmai90Decoder
+from apps.comunicator.services.handlers import Zmai90Decoder
 
 broker = '15.236.242.30'
 port = 1883
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 topic = "cmnd/tasmota_5A1C05/POWER"
+connector_id = "123456789"
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -17,10 +18,11 @@ def connect_mqtt():
         else:
             print("Failed to connect, return code %d\n", rc)
     # Set Connecting Client ID
-    client = mqtt_client.Client()
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+    connector = mqtt_client.Client(client_id=connector_id, clean_session=False, userdata=None)
+    connector.on_connect = on_connect
+    connector.max_inflight_messages_set(100)
+    connector.connect(broker, port)
+    return connector
 
 
 def publish(client):
@@ -40,12 +42,15 @@ def publish(client):
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print('\n' + f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-        Zmai90Decoder.decode(msg.payload.decode().split(':')[1][1:-1])
+        Zmai90De2coder.decode(msg.payload.decode().split(':')[1][1:-1])
 
     client.subscribe(topic)
     client.on_message = on_message
 
-client = connect_mqtt()
-result = client.publish(topic, "ON")
-status = result[0]
-print(status)
+
+def run(args):
+    connector = connect_mqtt()
+    connector.loop_forever()
+    result = connector.publish(topic, "ON")
+    status = result[0]
+    print(status)

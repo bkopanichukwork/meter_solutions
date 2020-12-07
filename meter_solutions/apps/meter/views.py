aeb92c27e43eeedfd1c88b84b599dcba3b1ab4e5
Bohdan Kopanichuk <danbog2000@gmail.com>
2020-12-07 18:42:33 +0200

    Added get_user_by_mail Endpoint

--- meter_solutions/apps/meter/views.py
+++ meter_solutions/apps/meter/views.py
@@ -3,8 +3,9 @@ import json
 from django.core import serializers
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from apps.meter.api.data_serializer import DataSerializer
